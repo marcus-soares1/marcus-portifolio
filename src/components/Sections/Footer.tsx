76f2dcb0374dce7060b8725ef2e84c2fcba1079a
Marcus Soares <marcus.silva.soares1@gmail.com>
2026-01-04 20:23:44 -0300

    Creating page footer

---
+++ src/components/Sections/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return (
+        <footer className="footer bg-dark text-light text-center py-4">
+            Desenvolvido por Marcus &lt;/&gt;
+        </footer>
+    )
+}
