2adaffd5cba82af75400ee51b00078630436e640
Marcus Soares <marcus.silva.soares1@gmail.com>
2026-01-04 22:35:41 -0300

    Creating projects section

---
+++ src/components/Sections/Projects.tsx
@@ -0,0 +1,12 @@
+export default function Projects ({children}: {children: any}) {
+    return (
+        <section id="projects" className="section-projects py-5">
+            <div className="container py-5">
+                <h2 className="text-primary mb-4">Projetos</h2>
+                <div className="d-flex flex-wrap justify-content-between gap-3">
+                    {children}
+                </div>
+            </div>
+        </section>
+    )
+}
